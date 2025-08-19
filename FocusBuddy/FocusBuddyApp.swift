92c9edba2e5b124bbdce3b4f95c2d6ce38f08cbf
Rana Taki <46297794+ranataki@users.noreply.github.com>
2025-08-19 13:49:43 -0700

    Initial Commit

---
+++ FocusBuddy/FocusBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusBuddyApp.swift
+//  FocusBuddy
+//
+//  Created by Rana Taki on 8/19/25.
+//
+
+import SwiftUI
+
+@main
+struct FocusBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FocusBuddyUITests/FocusBuddyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FocusBuddyUITestsLaunchTests.swift
+//  FocusBuddyUITests
+//
+//  Created by Rana Taki on 8/19/25.
+//
+
+import XCTest
+
+final class FocusBuddyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
